75aee4cd614aba27b064fc77cfa2ff2c05fa32ac
simao.lopes98 <simaolopes98@gmail.com>
2025-10-12 16:44:22 +0100

    Update main - Build passing ✅

--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
 /* main.c */
 
 #include <stdio.h>
+#include <stm32f1xx.h>
+#include <delay.h>
 
-int main ( void )
-    {
-    
-    printf ("Hello world :)\n");
-    return 0;
-    }
+int main(void)
+{
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	GPIOC->CRH = 0x02 << ((13 - 8) << 2);
+	while(1){
+		GPIOC->ODR ^= 1 << 13;
+		ms_delay(1000);
+	}
+}
