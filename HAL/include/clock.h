348affd2f7e64c7ffc2b977afca1f84d083d6208
simao.lopes98 <simaolopes98@gmail.com>
2026-02-08 18:24:46 +0000

    Add clock header

---
+++ HAL/include/clock.h
@@ -0,0 +1,21 @@
+/* Clock Header File */
+
+/* 
+ * File: clock.h
+ * Purpose: Clock hardware abstraction layer
+ */
+
+
+#ifndef CLOCK_H
+#define CLOCK_H
+
+/* Includes */
+#include <stdio.h>
+#include <stm32f1xx.h>
+#include <app.h>
+
+/* Macros */
+#define CLK_RATE 8000000
+/* Function Declarations */
+
+#endif /* CLOCK_H */
